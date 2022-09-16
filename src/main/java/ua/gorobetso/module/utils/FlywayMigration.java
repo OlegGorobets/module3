86ce95e1f7335614b8dd3d27a5f2275e27f85fb2
OlegGorobets <108553485+OlegGorobets@users.noreply.github.com>
2022-09-16 05:54:25 +0300

    fix.

--- src/main/java/ua/gorobetso/module/utils/FlywayMigration.java
+++ src/main/java/ua/gorobetso/module/utils/FlywayMigration.java
@@ -4,7 +4,7 @@ import org.flywaydb.core.Flyway;
 
 public class FlywayMigration {
     private static final Flyway flyway = Flyway.configure()
-            .dataSource("jdbc:postgresql://localhost:5432/module3", "postgres", "root")
+            .dataSource("jdbc:postgresql://ec2-44-205-112-253.compute-1.amazonaws.com:5432/dfofn8jefl1u07", "mshfngocxeqktw", "37ef35fa349adb0a63cd85610e7f80eeb1a5b2b27a0121b92b049865027ac5eb")
             .baselineOnMigrate(true)
             .locations("db/migration")
             .cleanDisabled(false)
